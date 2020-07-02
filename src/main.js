c2abb122e0e64e929262819005369a49e0dcb145
KIELVI <kielvi@gmail.com>
2020-07-01 21:45:13 -0300

    Eslint installed and configured

---
+++ src/main.js
@@ -0,0 +1,12 @@
+a = 3;
+const b = 2;
+
+const c = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+
+console.log(a, b, c);
+
+function sum() {
+	return a + b + c;
+}
+
+sum();
